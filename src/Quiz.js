48e54ce0ea7dd207f8e8dcfe65c475deb6d124bb
Prasanth Maddali <maddali.prasanth@gmail.com>
2019-04-04 15:48:00 +0800

    ss

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 let quizData = require('./quiz_data.json');
-class Quiz extends React.Component {
+
+class Quiz extends Component {
     constructor(props){
         super(props);
-        this.state = {quiz_position:0};
+        this.state = {quiz_position:1};
     }
 render(){
     return (
